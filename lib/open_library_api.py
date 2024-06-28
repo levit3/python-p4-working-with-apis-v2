aecadcea7340603160885d79814c629b3e35265c
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-06-28 18:13:30 +0300

    completes lab

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -4,33 +4,33 @@ import json
 
 class Search:
 
-    def get_search_results(self):
-        search_term = "the lord of the rings"
+    # def get_search_results(self):
+    #     search_term = "the lord of the rings"
 
-        search_term_formatted = search_term.replace(" ", "+")
-        fields = ["title", "author_name"]
-        # formats the list into a comma separated string
-        # output: "title,author_name"
-        fields_formatted = ",".join(fields)
-        limit = 1
+    #     search_term_formatted = search_term.replace(" ", "+")
+    #     fields = ["title", "author_name"]
+    #     # formats the list into a comma separated string
+    #     # output: "title,author_name"
+    #     fields_formatted = ",".join(fields)
+    #     limit = 1
 
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+    #     URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
 
-        response = requests.get(URL)
-        return response.content
+    #     response = requests.get(URL)
+    #     return response.content
 
-    def get_search_results_json(self):
-        search_term = "the lord of the rings"
+    # def get_search_results_json(self):
+    #     search_term = "the lord of the rings"
 
-        search_term_formatted = search_term.replace(" ", "+")
-        fields = ["title", "author_name"]
-        fields_formatted = ",".join(fields)
-        limit = 1
+    #     search_term_formatted = search_term.replace(" ", "+")
+    #     fields = ["title", "author_name"]
+    #     fields_formatted = ",".join(fields)
+    #     limit = 1
 
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-        print(URL)
-        response = requests.get(URL)
-        return response.json()
+    #     URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+    #     print(URL)
+    #     response = requests.get(URL)
+    #     return response.json()
 
     def get_user_search_results(self, search_term):
         search_term_formatted = search_term.replace(" ", "+")
